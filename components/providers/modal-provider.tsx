aaa374fc8dfe7305847da2fb1cc576a8a95fca12
n4ter88 <Nate.eizh@gmail.com>
2023-11-18 18:45:46 -0600

    delete model work

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -7,6 +7,7 @@ import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { MembersModel } from "@/components/modals/members-modal";
 import { CreateCHannelrModal } from "@/components/modals/create-channel-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -26,6 +27,7 @@ export const ModalProvider = () => {
         <EditServerModal />
         <MembersModel />
         <CreateCHannelrModal />
+        < LeaveServerModal />
         </>
     )
 }
