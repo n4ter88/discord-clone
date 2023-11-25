5e25e4f27f21d599a47c79951f124bbaf3aed105
n4ter88 <Nate.eizh@gmail.com>
2023-11-25 09:37:15 -0600

    setup conversations

--- lib/conversation.ts
+++ lib/conversation.ts
@@ -1,26 +1,40 @@
 import { db } from "@/lib/db";
 
+export const getOrCreateConversation = async (memberOneId: string, memberTwoId: string) => {
+    let conversation = await findConversation(memberOneId, memberTwoId) || await findConversation(memberTwoId, memberOneId);
+
+    if (!conversation) {
+        conversation = await createNewConversation(memberOneId, memberTwoId);
+    }
+
+    return conversation;
+}
+
 const findConversation = async (memberOneId: string, memberTwoId: string) => {
-    return await db.conversation.findFirst({
-        where: {
-            AND: [
-                { memberOneId: memberOneId },
-                { memberTwoId: memberTwoId },
-            ]
-        },
-        include: {
-            memberOne: {
-                include: {
-                    profile: true,
-                }
+    try {
+        return await db.conversation.findFirst({
+            where: {
+                AND: [
+                    { memberOneId: memberOneId },
+                    { memberTwoId: memberTwoId },
+                ]
             },
-            memberTwo: {
-                include: {
-                    profile: true,
+            include: {
+                memberOne: {
+                    include: {
+                        profile: true,
+                    }
+                },
+                memberTwo: {
+                    include: {
+                        profile: true,
+                    }
                 }
             }
-        }
-    });
+        });
+    }catch {
+    return null;
+    }
 }
 
 const createNewConversation = async (memberOneId: string, memberTwoId: string) => {
